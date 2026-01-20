a65573336fff6aca414f686ede8ba735bcc6d260
Dattatray Deulkar <Dattatray@hawkstack.com>
2026-01-20 15:05:42 +0530

    added the file and routing for openshift

---
+++ app/openshift/openshift/WhatWeDeliver.tsx
@@ -0,0 +1,9 @@
+export default function WhatWeDeliver() {
+  return (
+    <section className="py-20 bg-white text-center">
+      <h1 className="text-4xl font-bold text-gray-900">
+        What we deliver
+      </h1>
+    </section>
+  );
+}
---
+++ app/openshift/page.tsx
@@ -0,0 +1,12 @@
+import WhatWeDeliver from "@/components/home/WhatWeDeliver";
+import HeroSection from "../openshift/openshift/Herosection";
+
+export default function OpenShiftPage() {
+  return (
+    <>
+      <HeroSection />
+      <WhatWeDeliver />
+
+    </>
+  );
+}
