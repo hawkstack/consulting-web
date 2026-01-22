6754cb28f7ad41bd34108c25d0c4aec4fcfb7c16
Dattatray Deulkar <Dattatray@hawkstack.com>
2026-01-22 23:02:34 +0530

    feat: made responsive for all  screen with the aniamtion

--- app/openshift/page.tsx
+++ app/openshift/page.tsx
@@ -1,11 +1,13 @@
 import WhatWeDeliver from "@/components/openshift/WhatWeDeliver";
 import HeroSection from "@/components/openshift/HeroSection";
+import YourInfrastructure from "@/components/openshift/YourInfrastructure";
 
 export default function OpenShiftPage() {
   return (
     <>
       <HeroSection />
       <WhatWeDeliver />
+      <YourInfrastructure />
     </>
   );
 }
