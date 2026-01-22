a8b5d3fa0439821b06e3f310512754557735dbad
Dattatray Deulkar <Dattatray@hawkstack.com>
2026-01-22 15:37:36 +0530

    feat: added on hover shawdo for cards and chaged the font for tilte and description

--- components/openshift/WhatYouReceive.tsx
+++ components/openshift/WhatYouReceive.tsx
@@ -56,12 +56,12 @@ const WhatYouWillReceive: React.FC = () => {
       </div>
 
       {/* Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-full md:w-[855px] lg:w-[1200px] lg:h-[420px] mx-auto justify-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-full md:w-[855px] lg:w-[1200px] lg:h-[450px] mx-auto justify-items-center">
 
         {items.map((item: DeliverableItem, idx: number) => (
           <div
             key={idx}
-            className="bg-white rounded-2xl shadow-2xl flex flex-row md:flex-col gap-4 items-start w-[400px] max-w-full md:w-[325px] p-[18px] md:pt-[12px] md:pr-[6px] md:pb-[16px] md:pl-[16px] lg:w-[368px] ">
+            className="bg-white rounded-2xl shadow-2xl hover:shadow-[0_50px_60px_rgba(0,0,8,0.20)] flex flex-row md:flex-col gap-4 items-start w-[400px] max-w-full md:w-[325px] p-[18px] md:pt-[12px] md:pr-[6px] md:pb-[16px] md:pl-[16px] lg:w-[368px] lg:px-10">
             {/* Icon */}
             <Image
               src={item.icon}
@@ -72,10 +72,10 @@ const WhatYouWillReceive: React.FC = () => {
             />
             {/* Text */}
             <div className="overflow-hidden ">
-              <h3 className="text-[14px] font-medium text-black mb-1 leading-tight md:text-[13px] md:font-medium lg:text-20px">
+              <h3 className="text-[14px] font-medium text-black mb-1 leading-tight md:text-[15px] md:font-medium lg:text-20px">
                 {item.title}
               </h3>
-              <p className="text-[9px] font-normal text-black/70 leading-relaxed md:text-[8px] md:font-normal lg:text-14px">
+              <p className="text-[9px] font-normal text-black/70 leading-relaxed md:text-[10px] md:font-normal lg:text-14px">
                 {item.desc}
               </p>
             </div>
