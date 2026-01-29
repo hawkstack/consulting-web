85e2301b591c10d1d308cb55a06236f14a135ea2
Dattatray Deulkar <Dattatray@hawkstack.com>
2026-01-29 15:23:00 +0530

    feat: created the what we deliver section and added the images and constant file

--- app/openshift/unified/page.tsx
+++ app/openshift/unified/page.tsx
@@ -1,10 +1,12 @@
 import ChallengeSolution from "@/components/unified/ChallengeSolutionSection";
 import HeroSection from "@/components/unified/HeroSection";
+import WhatYouWillReceive from "@/components/unified/WhatYouReceive";
 export default function OpenShiftPage() {
   return (
     <>
       <HeroSection />
       <ChallengeSolution />
+      <WhatYouWillReceive />
     </>
   );
 }
