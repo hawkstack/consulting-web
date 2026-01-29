4eef7cd416703c617056cc9a0c9696953845ccd0
Dattatray Deulkar <Dattatray@hawkstack.com>
2026-01-29 16:11:08 +0530

    feat: created the your infrastructure section

--- app/openshift/unified/page.tsx
+++ app/openshift/unified/page.tsx
@@ -1,12 +1,14 @@
 import ChallengeSolution from "@/components/unified/ChallengeSolutionSection";
 import HeroSection from "@/components/unified/HeroSection";
 import WhatYouWillReceive from "@/components/unified/WhatYouReceive";
+import YourInfrastructure from "@/components/unified/YourInfrastructure";
 export default function OpenShiftPage() {
   return (
     <>
       <HeroSection />
       <ChallengeSolution />
       <WhatYouWillReceive />
+      <YourInfrastructure />
     </>
   );
 }
