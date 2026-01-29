e1e8def76a09dd502e9e0f52814b8699800cca95
Dattatray Deulkar <Dattatray@hawkstack.com>
2026-01-29 13:05:23 +0530

    feat/added what we deliver section for all screen

--- app/openshift/unified/page.tsx
+++ app/openshift/unified/page.tsx
@@ -1,8 +1,10 @@
+import ChallengeSolution from "@/components/unified/ChallengeSolutionSection";
 import HeroSection from "@/components/unified/HeroSection";
 export default function OpenShiftPage() {
   return (
     <>
       <HeroSection />
+      <ChallengeSolution />
     </>
   );
 }
