7a07c8fbd0c1165c66b232d72078b7469c3c6dbd
Gaurav Sharma <gaurav@hawkstack.com>
2026-02-06 17:49:07 +0530

    navbar logo issue fixed for larger screens

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -18,7 +18,7 @@ const menuMap: Record<MenuKey, MegaItem[] | null> = {
   solutions,
   services,
   company,
-  training
+  training,
 };
 
 export function Header(): JSX.Element {
@@ -32,13 +32,27 @@ export function Header(): JSX.Element {
     <header className="sticky top-0 z-51 w-full bg-gradient-to-r from-[#0A1B3F] via-[#0B2458] to-[#0A1B3F]">
       <nav className="relative w-full max-w-[1440px] mx-auto h-[72px] lg:h-[105px] px-4 md:px-8 lg:px-24 flex items-center">
         {/* LOGO */}
-        <div className="flex items-center justify-center w-[85.96px] h-[28.28px] md:w-[120px] md:h-[36px] lg:w-[152px] lg:h-[42px] rounded-[5.66px] md:rounded-[7px] lg:rounded-[7.92px] border border-white bg-white">
+        <div
+          className="
+          w-[85.96px] h-[28.28px]
+          md:w-[120px] md:h-[36px]
+          lg:w-[152px] lg:h-[42px]
+          xl:w-[170px] xl:h-[45px]
+          rounded-[5.66px]
+          md:rounded-[7px]
+          lg:rounded-[7.92px]
+          xl:rounded-[9px]
+          border border-white bg-white
+          flex items-center justify-center
+        "
+        >
           <Image
             src="/images/hawkstack-logo.webp"
             alt="HawkStack Logo"
-            width={124}
-            height={38}
+            width={170}
+            height={40}
             priority
+            className="object-contain"
           />
         </div>
 
