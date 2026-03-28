cb7e71267ce1ece999aab393f53bae5cfb58d0ac
gaurav-hawkstack <gaurav@hawkstack.com>
2026-03-28 16:39:16 +0530

    tablet mini view completed

--- components/home/Career/ProblemsSection.tsx
+++ components/home/Career/ProblemsSection.tsx
@@ -18,33 +18,33 @@ export default function ProblemsSection() {
   ];
 
   return (
-    <section className="relative bg-[#FFFFFF] xl:py-20 xl:px-32 lg:py-12 lg:px-6">
+    <section className="relative bg-[#FFFFFF] xl:py-20 xl:px-32 lg:py-12 lg:px-10 md:px-10 md:py-12">
       {/* Gradient Blur Shape */}
       <div className="pointer-events-none absolute bottom-[-100px] right-[-100px] w-[160px] h-[160px] opacity-90 blur-[220px] bg-gradient-to-br from-[#002EA3] to-[#009DFF] rounded-full" />
       {/* Heading */}
-      <h2 className="text-center xl:text-5xl lg:text-4xl font-medium text-[#0048FF] mb-16">
+      <h2 className="text-center xl:text-5xl lg:text-4xl md:text-3xl font-medium text-[#0048FF] mb-16">
         Are You Running Into These Problems
       </h2>
 
       {/* Cards */}
-      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 xl:gap-2 lg:gap-4">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 xl:gap-2 lg:gap-4 md:gap-2">
         {data.map((item) => (
           <div
             key={item.id}
-            className="border border-[#0048FF] rounded-4xl px-6 py-8 bg-[#FFFFFF] hover:shadow-md transition"
+            className="border border-[#0048FF] rounded-4xl xl:px-6 xl:py-8 lg:px-6 lg:py-8 md:px-4 md:py-4 bg-[#FFFFFF] hover:shadow-md transition"
           >
             {/* Number */}
-            <h3 className="text-blue-600 text-4xl font-medium mb-4">
+            <h3 className="text-blue-600 xl:text-4xl lg:text-4xl md:text-3xl font-medium mb-4">
               {item.id}
             </h3>
 
             {/* Title */}
-            <h4 className="font-medium xl:text-xl lg:text-lg mb-2">
+            <h4 className="font-medium xl:text-xl lg:text-lg md:text-base mb-2">
               {item.title}
             </h4>
 
             {/* Description */}
-            <p className="text-[#2F2F2F] font-normal text-sm leading-relaxed xl:mb-4">
+            <p className="text-[#2F2F2F] font-normal xl:text-sm lg:text-sm md:text-xs leading-relaxed xl:mb-4">
               {item.desc}
             </p>
           </div>
