0bd015ebaa058c5727ad8e11a792ffd053d44ab3
gaurav-hawkstack <gaurav@hawkstack.com>
2026-03-30 17:11:23 +0530

    Desktop view completed

--- components/home/Career/BenefitsSection.tsx
+++ components/home/Career/BenefitsSection.tsx
@@ -1,9 +1,46 @@
-const BenefitsSection = () => {
-  return (
-    <div>
-      <h1>BenefitsSection</h1>
-    </div>
-  );
-};
+import { benefits } from "./Benefitsdata";
 
-export default BenefitsSection;
+export default function BenefitsSection() {
+  return (
+    <section className="relative w-full bg-[#FFFFFF] xl:py-20 xl:px-30 overflow-hidden">
+      {/* Gradient Blur Shape */}
+      <div className="pointer-events-none absolute top-[80px] left-[100px] w-[150px] h-[150px] opacity-90 blur-[220px] bg-gradient-to-br from-[#002EA3] to-[#009DFF] rounded-full" />
+      <div className="max-full mx-auto text-left">
+        {/* Heading */}
+        <h2 className="mb-12">
+          <span className="text-[#0048FF] font-semibold xl:text-6xl">
+            Benefits
+          </span>{" "}
+          <span className="text-[#676767] xl:text-5xl font-medium ml-2">
+            that make a difference
+          </span>
+        </h2>
+
+        {/* Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+          {benefits.map((item, index) => {
+            const Icon = item.icon;
+            return (
+              <div
+                key={index}
+                className="bg-white rounded-2xl shadow-md border border-gray-100 p-6 text-center hover:shadow-lg transition"
+              >
+                {/* Icon */}
+                <div className="flex justify-center mb-4">
+                  <div className="p-3 rounded-xl bg-blue-50">
+                    <Icon className="text-blue-600 w-6 h-6" />
+                  </div>
+                </div>
+
+                {/* Text */}
+                <p className="text-[#000000] xl:text-base leading-relaxed">
+                  {item.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+}
