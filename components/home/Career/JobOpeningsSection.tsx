063593095e9f1fc31bf9874688612057316d9a34
Gaurav sharma <gaurav@hawkstack.com>
2026-03-31 19:01:24 +0530

    Tablet pro view added

--- components/home/Career/JobOpeningsSection.tsx
+++ components/home/Career/JobOpeningsSection.tsx
@@ -25,24 +25,24 @@ export default function JobOpeningsSection() {
       : jobs.filter((job) => job.category === activeTab);
 
   return (
-    <section className="w-full bg-[#FFFFFF] xl:py-18 xl:px-30">
+    <section className="w-full bg-[#FFFFFF] xl:py-18 xl:px-30 lg:py-16 lg:px-20 border">
       {/* Heading */}
       <div className="w-full mx-auto">
-        <h2 className="text-6xl font-semibold text-[#0048FF]">
+        <h2 className="xl:text-6xl lg:text-3xl font-semibold text-[#0048FF]">
           Be part of HawkStack
         </h2>
-        <p className="xl:mt-8 text-[#2F2F2F] font-medium xl:max-w-[64%] xl:text-xl">
+        <p className="xl:mt-8 lg:mt-4 text-[#2F2F2F] font-medium xl:max-w-[64%] xl:text-xl lg:text-lg">
           At HawkStack, we put people first from the very beginning of your
           journey with us. Here’s how our hiring process works:
         </p>
 
         {/* Tabs */}
-        <div className="flex flex-wrap gap-3 mt-6">
+        <div className="flex flex-wrap gap-3 xl:mt-6 lg:mt-6">
           {tabs.map((tab, i) => (
             <button
               key={i}
               onClick={() => setActiveTab(tab)}
-              className={`px-4 py-1.5 rounded-full font-medium xl:text-sm border ${
+              className={`px-4 py-1.5 rounded-full font-medium xl:text-sm lg:text-sm border ${
                 activeTab === tab
                   ? "bg-[#0048FF] text-white"
                   : "bg-white text-[#000000] border-[#2B2B2B]"
@@ -54,7 +54,7 @@ export default function JobOpeningsSection() {
         </div>
 
         {/* Job List */}
-        <div className="xl:mt-10 space-y-6 transition-all duration-300">
+        <div className="xl:mt-10 lg:mt-6 space-y-6 transition-all duration-300">
           {filteredJobs.map((job, i) => (
             <div
               key={i}
@@ -62,7 +62,7 @@ export default function JobOpeningsSection() {
             >
               {/* Left Content */}
               <div className="max-w-2xl">
-                <h3 className="text-[#0048FF] font-semibold xl:text-xl">
+                <h3 className="text-[#0048FF] font-semibold xl:text-xl lg:text-xl">
                   {job.title}
                 </h3>
                 <p className="text-[#2F2F2F] font-medium xl:text-sm xl:w-[90%] xl:mt-4">
@@ -71,11 +71,11 @@ export default function JobOpeningsSection() {
 
                 {/* Buttons */}
                 <div className="flex gap-3 mt-6">
-                  <button className="flex items-center gap-2 font-medium xl:px-4 xl:py-1.5 border border-[#2B2B2B] rounded-full xl:text-sm text-black hover:bg-gray-100">
+                  <button className="flex items-center gap-2 font-medium xl:px-4 xl:py-1.5 lg:px-3 lg:py-1 border border-[#2B2B2B] rounded-full xl:text-sm text-black hover:bg-gray-100">
                     <YourLocationIcon className="xl:w-4 xl:h-4" />
                     On-site
                   </button>
-                  <button className="flex items-center gap-2 font-medium xl:px-4 xl:py-1.5 border border-[#2B2B2B] rounded-full xl:text-sm text-black hover:bg-gray-100">
+                  <button className="flex items-center gap-2 font-medium xl:px-4 xl:py-1.5 lg:px-3 lg:py-1 border border-[#2B2B2B] rounded-full xl:text-sm text-black hover:bg-gray-100">
                     <YourClockIcon className="xl:w-4 xl:h-4" />
                     Full-time
                   </button>
