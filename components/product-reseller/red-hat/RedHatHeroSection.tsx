af8d43e9a00dd792895b68e6cefdd29e2a831bbf
Gaurav sharma <gaurav@hawkstack.com>
2026-05-04 11:53:10 +0530

    cursor pointer added.

--- components/product-reseller/red-hat/RedHatHeroSection.tsx
+++ components/product-reseller/red-hat/RedHatHeroSection.tsx
@@ -43,7 +43,7 @@ const RedHatHeroSection = ({ data }: Props) => {
                   block: "start",
                 });
               }}
-              className="bg-[#E40000] text-white px-4 py-2 lg:px-6 lg:py-3 rounded-md text-[11px] md:text-xs xl:text-sm font-medium transition"
+              className="bg-[#E40000] text-white px-4 py-2 lg:px-6 lg:py-3 rounded-md text-[11px] md:text-xs xl:text-sm font-medium transition cursor-pointer"
             >
               {ctaPrimary.text}
             </button>
