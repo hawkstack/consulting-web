0222e66c7084d3fbd700c75ad6830586312ef3ba
dattatray <dattatray@hawkstack.com>
2026-05-13 13:45:57 +0530

    resolved conflicts

--- app/product-reseller/redhat-enterprise-linux/page.tsx
+++ app/product-reseller/redhat-enterprise-linux/page.tsx
@@ -1,14 +1,9 @@
 import RedHatAICapabilitiesSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAICapabilitiesSection";
-<<<<<<< HEAD
 import RedHatAICtaSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAICtaSection";
 import RedHatAIFoundationSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIFoundationSection";
 import RedHatAIHeroSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIHeroSection";
 import RedHatAIOverviewSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIOverviewSection";
 import RedHatAIProductionSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIProductionSection";
-=======
-import RedHatAIHeroSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIHeroSection";
-import RedHatAIOverviewSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIOverviewSection";
->>>>>>> 84b9c7cb62e1c3eb80644673f838611aee705ec1
 import RedHatAIStrategySection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIStrategySection";
 import {
   redHatAIFoundationData,
