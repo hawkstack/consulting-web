38fb6aab825dd1155b6f624547c774e384f2abd5
dattatray-HS <dattatray@hawkstack.com>
2026-05-13 15:47:24 +0530

    solved the build  error (#429)

--- app/product-reseller/redhat-enterprise-linux/page.tsx
+++ app/product-reseller/redhat-enterprise-linux/page.tsx
@@ -1,6 +1,9 @@
 import RedHatAICapabilitiesSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAICapabilitiesSection";
+import RedHatAICtaSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAICtaSection";
+import RedHatAIFoundationSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIFoundationSection";
 import RedHatAIHeroSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIHeroSection";
 import RedHatAIOverviewSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIOverviewSection";
+import RedHatAIProductionSection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIProductionSection";
 import RedHatAIStrategySection from "@/components/product-reseller/product-redhat/redhat-ai/RedHatAIStrategySection";
 import {
   redHatAIFoundationData,
