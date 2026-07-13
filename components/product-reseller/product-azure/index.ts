ac93af690e26921d43fe7abc11be6ab917b18c7d
dattatray <dattatray@hawkstack.com>
2026-07-13 15:18:16 +0530

    added the test cases for the eng  section

--- components/product-reseller/product-azure/index.ts
+++ components/product-reseller/product-azure/index.ts
@@ -1,4 +1,13 @@
 export { default as AzureCompetenciesSection } from "./components/AzureCompetenciesSection";
+export { default as AzureEngineeringSection } from "./components/AzureEngineeringSection";
 export { default as AzureHeroSection } from "./components/AzureHeroSection";
-export { azureCompetenciesData, azureHeroData } from "./data/azure";
-export type { AzureCompetenciesData, AzureHeroData } from "./types/azure";
+export {
+  azureCompetenciesData,
+  azureEngineeringData,
+  azureHeroData,
+} from "./data/azure";
+export type {
+  AzureCompetenciesData,
+  AzureEngineeringData,
+  AzureHeroData,
+} from "./types/azure";
