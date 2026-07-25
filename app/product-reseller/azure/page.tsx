75ee01ff5343fa49527daff962bd7a11f1f5db71
Gaurav sharma <gaurav@hawkstack.com>
2026-07-25 12:01:32 +0530

    Build error fixed. (#569)

--- app/product-reseller/azure/page.tsx
+++ app/product-reseller/azure/page.tsx
@@ -5,6 +5,7 @@ import {
   AzureEngineeringSection,
   AzureHeroSection,
   AzureSkillsAccelerationSection,
+  azureBlueprintOperationalData,
   azureCompetenciesData,
   azureCtaData,
   azureEngineeringData,
